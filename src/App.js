3906f5663cacbc35fa66157d084f7d0eb941e7f7
kaaathi1211 <katharinakrotke.design@gmail.com>
2024-03-06 19:18:29 +1100

    Added Bootstrap to the project

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import './App.css';
 
 export default function App() {
   return (
-     <div className="App">Weather App
+     <div className="App">
+      <div className="container">
+      <h1>Weather App</h1>
      <footer>
-      This app was coded by Katharina Krotke and is 
-      <a href="https://github.com/kaaathi1211/weather-app-project" target="_blank" rel="noreferrer"> open-sourced on GitHub.</a>
+      This app was coded by{" "}
+      <a href="https://www.uxdesignbykk.com/" target="_blank" rel="noreferrer">Katharina Krotke</a>
+      {" "}and is{" "}
+      <a href="https://github.com/kaaathi1211/weather-app-project" target="_blank" rel="noreferrer">open-sourced on GitHub.</a>
       </footer>
+      </div>
       </div>
   );
 }
