f1975647e078b3e296b9abd1a2be353a4797be84
kaaathi1211 <katharinakrotke.design@gmail.com>
2024-03-06 18:54:15 +1100

    Added the GitHub repo url

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import './App.css';
 
 export default function App() {
   return (
-     <div className="App">Weather App</div>
+     <div className="App">Weather App
+     <footer>
+      This app was coded by Katharina Krotke and is 
+      <a href="https://github.com/kaaathi1211/weather-app-project" target="_blank" rel="noreferrer"> open-sourced on GitHub.</a>
+      </footer>
+      </div>
   );
 }
