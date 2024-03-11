5c9cecafb1577f90a6ea8b1b46ef3253caadb407
kaaathi1211 <katharinakrotke.design@gmail.com>
2024-03-11 21:08:51 +1100

    Added CSS styling to Weather component

--- src/Weather.js
+++ src/Weather.js
@@ -4,14 +4,14 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <h1>Weather Forecast</h1>
+      <h1>Your Weather Forecast</h1>
       <form>
         <div className="row">
           <div className="col-6">
             <input type="search" className="form-control" placeholder="Enter a city..." />
           </div>
           <div className="col-6">
-            <button type="submit" className="btn btn-primary">Search</button>
+            <button type="submit" className="btn btn-primary">SEARCH</button>
           </div>
         </div>
       </form>
@@ -19,17 +19,16 @@ export default function Weather() {
         <div className="col-6">
           <h4>Sydney</h4>
           <h3>30°C</h3>
-          <img src="./cloudy-weather.png" alt="Forecast" />
-          <ul>
+          <ul className="date">
             <li>Friday 8:00</li>
             <li>Mostly cloudy</li>
           </ul>
         </div>
         <div className="col-6">
           <ul>
-            <li>Humidity: 60%</li>
-            <li>Precipitation: 10%</li>
-            <li>Wind: 20km/h</li>
+            <li lassName="conditions">Humidity: 60%</li>
+            <li lassName="conditions">Precipitation: 10%</li>
+            <li lassName="conditions">Wind: 20km/h</li>
           </ul>
         </div>
       </div>
