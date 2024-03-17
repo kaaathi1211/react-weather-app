00c3d8fd6b072bacdee4fd9c5dd7b7528a6ad0a4
kaaathi1211 <katharinakrotke.design@gmail.com>
2024-03-17 17:15:00 +1100

    included axios, api and updated functions to show real time data

--- src/Weather.js
+++ src/Weather.js
@@ -1,44 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <h1>Your Weather Forecast</h1>
-      <form>
+  const [weatherData, setWeatherData] = useState({ready: false});
+  function handleResponse(response){
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      city: response.data.name,
+      date: "Friday, 7:00",
+      description: response.data.weather[0].description,
+      iconUrl: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F015%2F876%2F346%2Foriginal%2Fweather-icon-in-flat-style-sun-cloud-and-rain-illustration-on-white-isolated-background-meteorology-sign-business-concept-vector.jpg&tbnid=2bicBLEGUsCneM&vet=12ahUKEwiZzci80PqEAxV-g2MGHQxfCkMQMygAegQIARBx..i&imgrefurl=https%3A%2F%2Fwww.vecteezy.com%2Fvector-art%2F15876346-weather-icon-in-flat-style-sun-cloud-and-rain-vector-illustration-on-white-isolated-background-meteorology-sign-business-concept&docid=y_R5a4iX323kZM&w=1920&h=1920&q=weather%20icon&ved=2ahUKEwiZzci80PqEAxV-g2MGHQxfCkMQMygAegQIARBx",
+      wind: response.data.wind.speed,
+      precipitation: 20,
+      humidity: response.data.main.humidity,
+    });
+  }  
+  
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <h1>Your Weather Forecast</h1>
+        <form>
+          <div className="row">
+            <div className="col-6">
+              <input type="search" className="form-control" placeholder="Enter a city..." autoFocus="on" />
+            </div>
+            <div className="col-6">
+              <button type="submit" className="btn btn-primary">SEARCH</button>
+            </div>
+          </div>
+        </form>
         <div className="row">
           <div className="col-6">
-            <input type="search" className="form-control" placeholder="Enter a city..." autoFocus="on" />
-          </div>
-          <div className="col-6">
-            <button type="submit" className="btn btn-primary">SEARCH</button>
-          </div>
-        </div>
-      </form>
-      <div className="row">
-        <div className="col-6">
-          <h4>Sydney</h4>
-          
-          <span className="temperature">30</span>
-          <span className="units">°C</span>
-          </div>
-          
+            <h4>{weatherData.city}</h4>
+            
+            <span className="temperature">{Math.round(weatherData.temperature)}</span>
+            <span className="unit">°C</span>
+            </div>
+            
+           <img
+           src={weatherData.iconUrl}
+           alt={weatherData.description}
+           className="float-left"
+           />
 
+            <div className="d-flex flex-column">
+            <ul className="date">
+              <li>{weatherData.date}</li>
+              <li className="text-capitalize">{weatherData.description}</li>
+            </ul>
+            </div>
+          
           <div className="d-flex flex-column">
-          <ul className="date">
-            <li>Friday 8:00</li>
-            <li>Mostly cloudy</li>
-          </ul>
+            <ul className="conditions">
+              <li>Humidity: {weatherData.humidity}%</li>
+              <li>Precipitation: {weatherData.precipitation}%</li>
+              <li>Wind: {weatherData.wind}km/h</li>
+            </ul>
           </div>
-        
-        <div className="d-flex flex-column">
-          <ul className="conditions">
-            <li>Humidity: 60%</li>
-            <li>Precipitation: 10%</li>
-            <li>Wind: 20km/h</li>
-          </ul>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+  
+  else {
+    const apiKey = "515c9ddbeb3cda9061acfab71031839e";
+    let city = "Sydney";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city},uk&APPID=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
+
 }
