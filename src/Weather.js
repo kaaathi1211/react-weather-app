60faea4c5af45cbe4636b4aa217c9899dbb488ba
kaaathi1211 <katharinakrotke.design@gmail.com>
2024-03-23 13:21:19 +1100

    Added API function for real life weather updates for any city searched

--- src/Weather.js
+++ src/Weather.js
@@ -12,6 +12,7 @@ export default function Weather(props) {
   function handleResponse(response) {
     setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
       city: response.data.name,
       date: new Date(response.data.dt * 1000),
@@ -63,7 +64,7 @@ if (weatherData.ready) {
         </div>
       </form>
       <WeatherInfo data={weatherData} />
-      <WeatherForecast />
+      <WeatherForecast coordinates={weatherData.coordinates}/>
     </div>
   );
 } else {
